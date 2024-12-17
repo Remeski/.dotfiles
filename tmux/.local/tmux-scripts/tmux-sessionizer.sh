e8bc4dad155d75cd337951bf06a6f3dddfe39279
Remesk1 <eeliremes@gmail.com>
2024-12-17 15:21:58 +0200

    fixed bugs in sessionizer

--- tmux/.local/tmux-scripts/tmux-sessionizer.sh
+++ tmux/.local/tmux-scripts/tmux-sessionizer.sh
@@ -3,24 +3,29 @@
 # Polite yoink from ThePrimeagen .dotfiles :)
 
 if [[ $# -eq 1 ]]; then
-    selected=$1
+        selected=$1
 else
-	selected=$(find /mnt/storage ~/ ~/coding/ ~/coding/etremes  -mindepth 1 -maxdepth 5 -type d | fzf --reverse --print-query)
+	output=$(find /mnt/storage ~/ ~/coding/ ~/coding/etremes -mindepth 1 -maxdepth 5 -type d | fzf --reverse --print-query)
+	IFS=$'\n' read -r -d '' -a lines <<< "$output"  # Split output into lines
 
+	query=${lines[0]}  # Access the first line
+	selected=${lines[1]}  # Access the second line
+fi
+
+if [[ "${query:0:1}" == ">" ]]; then
+    selected=${selected:1}
+    selected_name=$selected
 fi
 
 if [[ -z $selected ]]; then
     exit 0
 fi
 
-selected_name=$(basename "$selected" | tr . _)
-
-if [[ "${selected:0:1}" == ">" ]]; then
-    selected=${selected:1}
-    selected_name=$selected
-fi
+selected_name=$(basename $selected | tr . _)
 
 tmux_running=$(pgrep tmux)
+
+zoxide add $selected
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
     tmux new-session -s $selected_name -c $selected
