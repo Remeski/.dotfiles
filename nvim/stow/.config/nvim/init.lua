0eca4a19da58c5813bd4e2f61c95479ef777c385
Remesk1 <eeliremes@gmail.com>
2025-11-16 11:30:24 +0200

    tweaks

--- nvim/stow/.config/nvim/init.lua
+++ nvim/stow/.config/nvim/init.lua
@@ -24,26 +24,25 @@ vim.pack.add({
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/saghen/blink.cmp" },
-	{ src = "https://github.com/shaunsingh/nord.nvim" },
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
 	{ src = "https://github.com/tpope/vim-fugitive" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" }
 })
 
-vim.g.nord_disable_background = true
-vim.cmd[[colorscheme nord]]
 
 require("telescope").setup()
 require("mason").setup()
 require("oil").setup()
-require("typst-preview").setup()
+require("typst-preview").setup({
+	open_cmd = "firefox %s"
+})
 require("blink.cmp").setup({
 	fuzzy = { implementation = "rust" }
 })
 require("nvim-treesitter").setup()
 -- require("vim-fugitive")
 
-vim.lsp.enable({ "lua_ls", "tinymist", "pyright", "rust_analyzer"})
+vim.lsp.enable({ "lua_ls", "tinymist", "pyright", "rust_analyzer", "ts_ls" })
 vim.lsp.config('*', {})
 
 local telescope = require('telescope.builtin')
@@ -65,3 +64,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 vim.keymap.set("n", "<ESC>", "<CMD>nohlsearch<CR><ESC>")
+
+-- theme
+vim.pack.add({
+	-- { src = "https://github.com/shaunsingh/nord.nvim" }
+	{ src = "https://github.com/ellisonleao/gruvbox.nvim" }
+})
+
+-- vim.g.nord_disable_background = true
+
+vim.cmd[[colorscheme gruvbox]]
