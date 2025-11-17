b9a8dc60879c14d5c2831dc9ba85b76ff65662f0
Remesk1 <eeliremes@gmail.com>
2025-11-17 13:56:29 +0200

    tmux colors

--- nvim/stow/.config/nvim/colors.lua
+++ nvim/stow/.config/nvim/colors.lua
@@ -1,45 +1,45 @@
 -- local config = {
---   base00 = "#0f141a",
---   base01 = "#0b0c0e",
---   base02 = "#dfbbde",
---   base03 = "#17191c",
---   base04 = "#0a0c0f",
---   base05 = "#dfe3eb",
---   base06 = "#0a0c10",
---   base07 = "#0f141a",
+--   base00 = "#141318",
+--   base01 = "#0c0c0e",
+--   base02 = "#c8c2e6",
+--   base03 = "#19181b",
+--   base04 = "#0c0b0e",
+--   base05 = "#e5e1e8",
+--   base06 = "#0d0b0e",
+--   base07 = "#141318",
 --   base08 = "#1a0300",
---   base09 = "#a5caf5",
---   base0A = "#c3cd72",
---   base0B = "#d0e4ff",
---   base0C = "#c3cd72",
---   base0D = "#171a00",
---   base0E = "#dfea8b",
---   base0F = "#dfe3eb",
+--   base09 = "#ffade1",
+--   base0A = "#c7bfff",
+--   base0B = "#ffd8ed",
+--   base0C = "#c7bfff",
+--   base0D = "#0a0910",
+--   base0E = "#ffffff",
+--   base0F = "#e5e1e8",
 -- }
 -- require('base16-colorscheme').setup(config)
 
 require('base16-colorscheme').setup({
-  base00 = "#0f141a",
-  base01 = "#0a0f14",
-  base02 = "#171c22",
-  base03 = "#6b798d",
-  base04 = "#bec7d5",
-  base05 = "#dfe3eb",
-  base06 = "#dfe3eb",
-  base07 = "#556070",
+  base00 = "#141318",
+  base01 = "#0e0e12",
+  base02 = "#1c1b20",
+  base03 = "#787487",
+  base04 = "#c9c4d2",
+  base05 = "#e5e1e8",
+  base06 = "#e5e1e8",
+  base07 = "#5f5d6b",
   base08 = "#ffb4ab",
-  base09 = "#cf98cd",
-  base0A = "#c3cd72",
-  base0B = "#c3cd72",
-  base0C = "#dfbbde",
-  base0D = "#a5caf5",
-  base0E = "#fcd7fb",
+  base09 = "#a79ed7",
+  base0A = "#c7bfff",
+  base0B = "#c7bfff",
+  base0C = "#c8c2e6",
+  base0D = "#ffade1",
+  base0E = "#e2dcff",
   base0F = "#ffdad6",
 })
 
 vim.api.nvim_set_hl(0, 'Visual', {
-  bg = '#434b00',
-  fg = '#0f141a',
+  bg = '#645ba5',
+  fg = '#141318',
 })
 
 -- vim.api.nvim_set_hl(0, 'Comment', {
@@ -48,7 +48,7 @@ vim.api.nvim_set_hl(0, 'Visual', {
 -- })
 
 vim.api.nvim_set_hl(0, 'String', {
-	fg = "#a5caf5"
+	fg = "#ffade1"
 })
 
 -- vim.api.nvim_set_hl(0, '@comment', {
@@ -57,5 +57,5 @@ vim.api.nvim_set_hl(0, 'String', {
 -- })
 
 vim.api.nvim_set_hl(0, '@string', {
-	fg = "#a5caf5"
+	fg = "#ffade1"
 })
