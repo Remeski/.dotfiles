54b6ec2b394aef30950b9539ee91fdfbe21398f8
Remesk1 <eeliremes@gmail.com>
2026-07-15 23:37:42 +0300

    dwl: improved startup script

--- dwl/stow/.local/scripts/start-dwl.sh
+++ dwl/stow/.local/scripts/start-dwl.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 if [ -n "$1" ]; then
+	systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE
+	dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE
+	systemctl --user start xdg-desktop-portal-wlr
+
 	swaybg -m fill -i /home/rmz/Pictures/Bgs/blackhole.jpeg &
 	hypridle &
 	dunst &
 else
-	slstatus -s | dwl -s './.local/scripts/start-dwl.sh startup'
+	slstatus -s | dbus-run-session dwl -s './.local/scripts/start-dwl.sh startup'
 fi
